e51b595d76e31c4e5f8cacb205bf59369ee272ef
Maradnus <155420042+Maradnus@users.noreply.github.com>
2025-04-26 17:04:31 +0530

    Add files via upload

--- system_init.h
+++ system_init.h
@@ -1,23 +1,15 @@
-/* 
- * File:   system_init.h
- * Author: shivasundaram
- *
- * Created on 26 April, 2025, 2:34 PM
- */
-
 #ifndef SYSTEM_INIT_H
-#define	SYSTEM_INIT_H
+#define SYSTEM_INIT_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+#include <avr/io.h>
+#include <stdint.h>
 
+// Function prototypes
+void Sys_Init(void);
+void Sys_InitPower(void);
+void Sys_InitCAN(void);
+void Sys_InitMode(void);
+void Sys_InitSolenoid(void);
+void Sys_InitMonitor(void);
 
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* SYSTEM_INIT_H */
-
+#endif /* SYSTEM_INIT_H */
